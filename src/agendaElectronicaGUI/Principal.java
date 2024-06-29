b52f05fd4b1b38a336554eaf9758e60c09a1a860
Martin Guevara <guevaramartinjavier@gmail.com>
2024-06-29 12:33:55 -0300

    Disenio inicial

--- src/agendaElectronicaGUI/Principal.java
+++ src/agendaElectronicaGUI/Principal.java
@@ -3,4 +3,22 @@ package agendaElectronicaGUI;
 import javax.swing.*;
 
 public class Principal {
+    private JLabel txtTitulo;
+    private JLabel lblDNI;
+    private JTextField txtDNI;
+    private JLabel lblDir;
+    private JTextField txtDir;
+    private JLabel lblNomb;
+    private JTextField txtNomb;
+    private JLabel lblTel;
+    private JTextField txtTel;
+    private JLabel lblApe;
+    private JTextField txtApe;
+    private JLabel lblFecha;
+    private JTextField txtFecha;
+    private JButton btnRetr;
+    private JButton btnGuar;
+    private JButton btnAvan;
+    private JLabel lblInd;
+    private JTextField txtInd;
 }
